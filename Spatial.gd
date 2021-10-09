8bf2f8aa1e0f4c56f423fdb52986b93e77fbcd63
Sam Alws <sam@samalws.com>
2021-10-09 16:21:25 -0400

    cool solar system

--- Spatial.gd
+++ Spatial.gd
@@ -1,25 +1,42 @@
 extends Spatial
 
+var G = 1000
 
 var velocitiesDict = {}
 var massDict = {}
 
-func _ready():
-	initDicks($Sun,0,0,0,500)
-	initDicks($GooberPlanet,-4,4,0,25)
-	initDicks($ChadPlanet,0,-6,6,12)
-	$GooberPlanet/CSGSphere/AudioStreamPlayer3D.playing = true;
+func velCalc(M,r):
+	return sqrt(G*M/r)
 
-var G = 10
+func centerOfMass():
+	var total = Vector3()
+	var totalMass = 0
+	for planet in velocitiesDict.keys():
+		total += planet.translation*massDict[planet]
+		totalMass += massDict[planet]
+	return total/totalMass
+
+func _ready():
+	initDicks($Sun1,0,0,velCalc(250,45)/2,251)
+	initDicks($Sun2,0,0,-velCalc(250,45)/2,249)
+	initDicks($Planet1,velCalc(500,200),0,0,10)
+	initDicks($Planet2,velCalc(500,400),0,0,15)
+	initDicks($Planet3,velCalc(500,800),0,0,30)
+	initDicks($Moon21,velCalc(500,400),0,velCalc(15,25),.2)
+	initDicks($Moon31,velCalc(500,800),0,velCalc(30,40),.03)
+	initDicks($Moon32,velCalc(500,800),0,velCalc(30,50),.01)
+	initDicks($Moon33,velCalc(500,800),0,velCalc(30,55),.01)
 
 func _process(delta):
 	for planet in velocitiesDict.keys():
 		for planet2 in velocitiesDict.keys():
 			fuckYou(planet,planet2, delta)
-		planet.translation += velocitiesDict[planet]*delta*10
-	$Camera.translation = $Sun.translation
-	$Camera.translation.y += 300
-	$Camera.translation.x -= 250
+	for planet in velocitiesDict.keys():
+		planet.translation += velocitiesDict[planet]*delta
+	$Camera.translation = centerOfMass()
+	$Camera.translation.y += 1600
+	#$Camera.translation.x -= 250
+	checkAllCollisions()
 	
 	
 	
@@ -33,6 +50,36 @@ func fuckYou(thisPlanet, otherPlanet, delta):
 		return
 	var diff = otherPlanet.translation-thisPlanet.translation
 	var distance = diff.length_squared()
-	var force = (G*massDict[thisPlanet]*massDict[otherPlanet]/distance)*(diff.normalized())
-	var accel = force/massDict[thisPlanet]
-	velocitiesDict[thisPlanet] += accel*delta*10
+	var accel = (G*massDict[otherPlanet]/distance)*(diff.normalized())
+	velocitiesDict[thisPlanet] += accel*delta
+
+func checkAllCollisions():
+	for planet1 in velocitiesDict.keys():
+		for planet2 in velocitiesDict.keys():
+			checkCollisions(planet1,planet2)
+
+func getSphere(planet):
+	return planet.get_node("CSGSphere")
+
+func checkCollisions(thisPlanet,otherPlanet):
+	if(thisPlanet == otherPlanet or not massDict.has(thisPlanet) or not massDict.has(otherPlanet)):
+		return
+	var dist = thisPlanet.translation.distance_to(otherPlanet.translation)
+	var radiusSum = getSphere(thisPlanet).radius + getSphere(otherPlanet).radius
+	if dist < radiusSum * .5:
+		var planetToKill = thisPlanet
+		var planetToKeep = otherPlanet
+		if massDict[thisPlanet] > massDict[otherPlanet]:
+			planetToKill = otherPlanet
+			planetToKeep = thisPlanet
+		var killMass = massDict[planetToKill]
+		var keepMass = massDict[planetToKeep]
+		var totalMass = keepMass + killMass
+		# preserve density:
+		getSphere(planetToKeep).radius = pow(pow(getSphere(planetToKeep).radius,3) * totalMass / keepMass, 1.0/3)
+		# preserve momentum:
+		velocitiesDict[planetToKeep] = ((velocitiesDict[planetToKeep]*keepMass)+(velocitiesDict[planetToKill]*killMass))/totalMass
+		massDict[planetToKeep] += killMass
+		massDict.erase(planetToKill)
+		velocitiesDict.erase(planetToKill)
+		planetToKill.queue_free()
