311048c9d015ddd954a0285daed9b32aab9e7d06
[your <[Your>
2021-10-09 00:47:23 -0500

    Sam is a nerd, made physics cool !!

--- Spatial.gd
+++ Spatial.gd
@@ -1,25 +1,38 @@
 extends Spatial
 
 
-# var velocitiesDict = {} shut up sam
+var velocitiesDict = {}
+var massDict = {}
 
 func _ready():
-	pass
-	#for planet in self.get_children():
-		#velocitiesDict[planet] = Vector3() shut up sam
+	initDicks($Sun,0,0,0,500)
+	initDicks($GooberPlanet,-4,4,0,25)
+	initDicks($ChadPlanet,0,-6,6,12)
+	$GooberPlanet/CSGSphere/AudioStreamPlayer3D.playing = true;
 
-
-var gooberPlanetVelocity = Vector3(10,0,0)
-var G = 5000
+var G = 10
 
 func _process(delta):
-	var thisPlanet = $GooberPlanet
-	var otherPlanet = $Sun
+	for planet in velocitiesDict.keys():
+		for planet2 in velocitiesDict.keys():
+			fuckYou(planet,planet2, delta)
+		planet.translation += velocitiesDict[planet]*delta*10
+	$Camera.translation = $Sun.translation
+	$Camera.translation.y += 300
+	$Camera.translation.x -= 250
+	
+	
+	
+
+func initDicks(planet,x,y,z,mass):
+	velocitiesDict[planet] = Vector3(x,y,z)
+	massDict[planet] = mass
+	
+func fuckYou(thisPlanet, otherPlanet, delta):
+	if(thisPlanet == otherPlanet):
+		return
 	var diff = otherPlanet.translation-thisPlanet.translation
 	var distance = diff.length_squared()
-	var force = (G*1*1/distance)*(diff.normalized())
-	gooberPlanetVelocity += force*delta*10
-	$GooberPlanet.translation += gooberPlanetVelocity*delta*10
-	print(gooberPlanetVelocity)
-	#print($GooberPlanet.translation)
-	
+	var force = (G*massDict[thisPlanet]*massDict[otherPlanet]/distance)*(diff.normalized())
+	var accel = force/massDict[thisPlanet]
+	velocitiesDict[thisPlanet] += accel*delta*10
