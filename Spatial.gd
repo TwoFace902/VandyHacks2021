18b26331b7c3d56db2b2db5144aa4381d8abfa61
[your <[Your>
2021-10-10 00:07:08 -0500

    Redid skybox, added textures, added camera stuff

--- Spatial.gd
+++ Spatial.gd
@@ -4,37 +4,42 @@ var ticFrame = 0
 var G = 1000
 var velocitiesDict = {}
 var massDict = {}
+var objects = []
+onready var currentPlanet = $DefaultCam
 
 func _ready():
+	$CamSpat/Camera.fov = 1
 	initDict($Sun,0,0,0,500)
 	initDict($GooberPlanet,-40,40,0,25)
-	initDict($MoonPog,-40,40,sqrt(G*25/15),0.000000000001)
+	initDict($MoonPog,-40,40,velCalc(25,15),0.000000000001)
 	initDict($ChadPlanet,0,-30,30,12)
 	initDict($Supergoober,30,2,30,16)
-	print($Sun.get_children()[0].radius)
+	initDict($DefaultCam,velCalc(500,350),0,0,0.1)
 
 func _process(delta):
+	if($CamSpat/Camera.fov <= 70):
+		$CamSpat/Camera.fov += 0.5
 	for planet in velocitiesDict.keys():
 		for planet2 in velocitiesDict.keys():
 			calcVel(planet,planet2, 0.005)
 	for planet in velocitiesDict.keys():
 		planet.translation += velocitiesDict[planet]*0.005
-		if(ticFrame%30 == 0):
-			$Trajectory.createTraject(planet.translation)
 		planet.rotation.y += delta
+		if(ticFrame%30 == 0 && planet != $Sun && planet != $DefaultCam):
+			$Trajectory.createTraject(planet.translation)
+		else :
+			var looking = (currentPlanet.translation - $Sun.translation).normalized()
+			$CamSpat.translation = currentPlanet.translation
+			$CamSpat.look_at($Sun.translation,Vector3(0,0,1))
 	$Trajectory.translation = $Sun.translation
-	$Camera.translation = centerOfMass()
-	#$Camera.rotation.x += 0.01
-	$Camera.translation.y +=350
-	#$Camera.translation.x -= 150
-	#$Camera.rotation = $GooberPlanet.rotation
-	#$Camera.rotation.y += 180
 	ticFrame += 1
 	checkAllCollisions()
 
 func initDict(planet,x,y,z,mass):
 	velocitiesDict[planet] = Vector3(x,y,z)
 	massDict[planet] = mass
+	if(planet != $Sun):
+		objects.push_back(planet)
 	
 func calcVel(thisPlanet, otherPlanet, delta):
 	if(thisPlanet == otherPlanet):
@@ -50,7 +55,8 @@ func getSphere(planet):
 func checkAllCollisions():
 	for planet1 in velocitiesDict.keys():
 		for planet2 in velocitiesDict.keys():
-			checkCollisions(planet1,planet2)
+			if(planet1 != $DefaultCam && planet2 != $DefaultCam):
+				checkCollisions(planet1,planet2)
 
 func checkCollisions(thisPlanet,otherPlanet):
 	if(thisPlanet == otherPlanet or not massDict.has(thisPlanet) or not massDict.has(otherPlanet)):
@@ -73,6 +79,9 @@ func checkCollisions(thisPlanet,otherPlanet):
 		massDict[planetToKeep] += killMass
 		massDict.erase(planetToKill)
 		velocitiesDict.erase(planetToKill)
+		objects.erase(planetToKill)
+		if(currentPlanet == planetToKill):
+			nextPlanet()
 		planetToKill.queue_free()
 
 func velCalc(M,r):
@@ -85,3 +94,19 @@ func centerOfMass():
 		total += planet.translation*massDict[planet]
 		totalMass += massDict[planet]
 	return total/totalMass
+
+func nextPlanet():
+	currentPlanet.visible = true
+	currentPlanet = objects[(objects.find(currentPlanet,0) + 1)%objects.size()]
+	currentPlanet.visible = false
+	
+func prevPlanet():
+	currentPlanet.visible = true
+	currentPlanet = objects[(objects.find(currentPlanet,0) - 1)%objects.size()]
+	currentPlanet.visible = false
+	
+func _input(ev):
+	if Input.is_action_just_pressed("ui_right"):
+		nextPlanet()
+	if Input.is_action_just_pressed("ui_left"):
+		prevPlanet()
--- TrajectKid.tres.gd
+++ TrajectKid.tres.gd
@@ -8,13 +8,22 @@ var lifetime = 20 #what?
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	lifetime -= delta
-	if(lifetime <= 0.25):
+	#if(lifetime <= 19.75):
+	#	visible = true
+	if (lifetime <= 19.75 and lifetime >= 19.5):
+		visible = true
+		var timeAlongProcess = 19.75 - lifetime
+		var toWhite = timeAlongProcess/.25
+		material.albedo_color = Color(toWhite,toWhite,toWhite,255)
+	elif (lifetime >= 0.25 and lifetime <= 19.5):
+		material.albedo_color = Color(1,1,1,255)
+	elif (lifetime <= 0.25):
 		var toBlack = 80*(lifetime/20)
 		material.albedo_color = Color(toBlack,toBlack,toBlack,255)
 	if(lifetime <= 0):
