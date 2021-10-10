ba22d6bedd3e0936a320804df5b054a93a302eb7
[your <[Your>
2021-10-10 00:45:08 -0500

    Did some weird stuff, need to start cleanup soon

--- TrajectKid.tres.gd
+++ TrajectKid.tres.gd
@@ -1,6 +1,6 @@
 extends CSGSphere
 
-var lifetime = 20
+var lifetime = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,15 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	lifetime -= delta
-	if (lifetime <= 19.75 and lifetime >= 19.5):
+	if (lifetime <= 29.75 and lifetime >= 29.5):
 		visible = true
-		var timeAlongProcess = 19.75 - lifetime
+		var timeAlongProcess = 29.75 - lifetime
 		var toWhite = timeAlongProcess/.25
 		material.albedo_color = Color(toWhite,toWhite,toWhite,255)
-	elif (lifetime > 0.25 and lifetime < 19.5):
+	elif (lifetime > 0.25 and lifetime < 29.5):
 		material.albedo_color = Color(1,1,1,255)
 	elif (lifetime <= 0.25):
-		var toBlack = 80*(lifetime/20)
+		var toBlack = lifetime/0.25
 		material.albedo_color = Color(toBlack,toBlack,toBlack,255)
 	if(lifetime <= 0):
 		queue_free()
